32dc8cc8a1ff9620cc45c11c17c6ce8f909eabb5
devZahid <officialzahidhasan19932021@gmail.com>
2023-08-10 14:14:11 +0600

    i have developed contact section for our homepage

--- src/components/pages/homepage/Homepage.jsx
+++ src/components/pages/homepage/Homepage.jsx
@@ -1,11 +1,9 @@
 import BannerSection from "./BannerSection";
-import Contact from "./Contact";
 
 const Homepage = () => {
   return (
     <div className="min-h-screen">
       <BannerSection></BannerSection>
-      <Contact></Contact>
     </div>
   );
 };
