95fe18e28d1a8db47cc82797cbe8f3432fc74f21
devZahid <officialzahidhasan19932021@gmail.com>
2023-08-10 14:11:01 +0600

    i have developed contact page for the phoenix homepage

--- src/components/pages/homepage/Homepage.jsx
+++ src/components/pages/homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import BannerSection from "./BannerSection";
+import Contact from "./Contact";
 
 const Homepage = () => {
   return (
     <div className="min-h-screen">
       <BannerSection></BannerSection>
+      <Contact></Contact>
     </div>
   );
 };
